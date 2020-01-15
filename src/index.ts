41afdd0474a1b82df354930696f13d36424aa9e2
Alec LaLonde <alec@convergencelabs.com>
2020-01-15 14:43:34 -0700

    Added customization of NavigationPlugin to fix #1

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
 import { Application } from "typedoc/dist/lib/application";
-import { CustomModulesPlugin } from "./plugin";
+import { CustomModulesPlugin } from "./CustomModulesPlugin";
+import { CustomModulesNavPlugin } from './CustomModulesNavPlugin';
 
 export = (PluginHost: Application) => {
   const app = PluginHost.owner;
 
   app.converter.addComponent("custom-modules", new CustomModulesPlugin(app.converter));
+
+  app.renderer.removeComponent("navigation");
+  app.renderer.addComponent("navigation", new CustomModulesNavPlugin(app.renderer));
 };
